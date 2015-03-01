d7be2567161117793e7fc49ddf2444a62fd15d15
Kirill Feoktistov <fekirill@gmail.com>
2015-03-02 01:37:15 +0300

    Programming assignment 1 finished

---
+++ Assignment1/AsyncTaskLab/submit.sh
@@ -0,0 +1,6 @@
+rm -rf AsyncTaskLabSubmit
+mkdir AsyncTaskLabSubmit
+mkdir AsyncTaskLabSubmit/AsyncTaskLab
+cp Lab5-AsyncTaskLab/src/course/labs/asynctasklab/DownloaderTaskFragment.java ./AsyncTaskLabSubmit/AsyncTaskLab/
+zip -r0 AsyncTaskLabSubmit.zip AsyncTaskLabSubmit/*
+rm -rf AsyncTaskLabSubmit
