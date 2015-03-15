4fce58418038e24cd00453d022e31e1085b6a9eb
Kirill Feoktistov <fekirill@gmail.com>
2015-03-16 02:11:47 +0300

    Programming assignment 3 finished

---
+++ Assignment3/GraphicsAndTouch/submit.sh
@@ -0,0 +1,6 @@
+rm -rf GraphicsLabSubmit
+mkdir GraphicsLabSubmit
+mkdir GraphicsLabSubmit/GraphicsLab
+cp Lab7_GraphicsLab/src/course/labs/graphicslab/BubbleActivity.java ./GraphicsLabSubmit/GraphicsLab
+zip -r0 GraphicsLabSubmit.zip GraphicsLabSubmit/*
+rm -rf GraphicsLabSubmit
